0abf0e86c459ed97dcbf7813c1d4c02081aa3c0c
Victor Ernesto Regalado Gonzalez <66405916+victoregonzalez01@users.noreply.github.com>
2025-07-21 19:42:18 -0700

    Update script.js
    se elimina mensaje de "Error al cargar productos: cannot set properties of null (setting 'innerHTML')"

--- script.js
+++ script.js
@@ -17,10 +17,7 @@ async function cargarProductos() {
         const data = await response.json();
         productos = data.productos;
         mostrarProductosDisponibles();
-    } catch (error) {
-        console.error('Error:', error);
-        mostrarError(`Error al cargar productos: ${error.message}`);
-    }
+    } 
 }
 
 // Mostrar productos disponibles
